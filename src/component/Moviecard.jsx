53fc9b86589547f8e391c7bb92feb9daf34d4b68
jaysaha01 <sahajay426@gmail.com>
2025-04-26 21:05:27 +0530

    third commit

--- src/component/Moviecard.jsx
+++ src/component/Moviecard.jsx
@@ -13,7 +13,7 @@ const Moviecard = ({ mydata }) => {
     <>
       <Link to={`/browse/fullview/${id}`}>
         <div className='moviecard'>
-          <img src={poster_path ? `http://image.tmdb.org/t/p/w500/${poster_path}` || `http://image.tmdb.org/t/p/w500/${backdrop_path}` : "https://drive-in-theatre.netlify.app/movieImages/default-movie.png"} alt="" srcset="" />
+          <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` || `http://image.tmdb.org/t/p/w500/${backdrop_path}` : "https://drive-in-theatre.netlify.app/movieImages/default-movie.png"} alt="" srcset="" />
           <div className="overlybx">
             <div className="header">
               <AddCircleIcon />
